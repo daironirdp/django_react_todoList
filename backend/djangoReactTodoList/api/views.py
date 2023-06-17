9f7f263fc4ae3d1599a3c3bb4112e627ebe2e48a
daironirp <daironirp@estudiantes.uci.cu>
2023-06-17 17:52:51 -0500

    commiting the development branch and adding some todos features in the backend.

---
+++ backend/djangoReactTodoList/api/views.py
@@ -0,0 +1,59 @@
+from rest_framework import generics, permissions
+from .serializers import TodoSerializer, TodoToggleCompleteSerializer
+from todo.models import Todo
+
+
+class TodoList(generics.ListAPIView):
+
+    # ListAPIView requires two mandatory attributes, serializer_class and queryset.
+    # We specify TodoSerializer which we have earlier implemented
+
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
+
+
+class TodoListCreate(generics.ListCreateAPIView):
+
+    # ListAPIView requires two mandatory attributes, serializer_class and queryset.
+    # We specify TodoSerializer which we have earlier implemented
+
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        # serializer holds a django model
+        serializer.save(user=self.request.user)
+
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # user can only update, delete own posts
+        return Todo.objects.filter(user=user)
+
+# views for customised logic
+
+
+class TodoToggleComplete(generics.UpdateAPIView):
+
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
+
+    def perform_update(self, serializer):
+        serializer.instance.completed = not (serializer.instance.completed)
+        serializer.save()
